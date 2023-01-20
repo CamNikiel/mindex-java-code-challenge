849abbac21a89738f77c41a73f96beea2b58bfaf
CamNikiel <123035555+CamNikiel@users.noreply.github.com>
2023-01-20 12:22:01 -0500

    Update ReportingStructure.java

--- src/main/java/com/mindex/challenge/data/ReportingStructure.java
+++ src/main/java/com/mindex/challenge/data/ReportingStructure.java
@@ -1,19 +1,42 @@
+package com.mindex.challenge.data;
 
-class ReportingStructure {
+import java.util.List;
+
+public class ReportingStructure {
 
     private Employee employee;
     private int numOfReports;
+    private int totalReports = 0;
 
-    public ReportingStructure(Employee employee, int numOfReports){
+    public ReportingStructure(Employee employee){
         this.employee = employee;
-        this.numOfReports = numOfReports;
     }
 
     public Employee getEmployee(){
         return employee;
     }
 
+    public void setEmployee(){
+        this.employee = employee;
+    }
+
     public int getNumOfReports(){
         return numOfReports;
+    }
+
+    public void setNumOfReports(){
+        this.numOfReports = findNumReports(this.employee);
+    }
+
+    public int findNumReports(Employee employee){
+        List<Employee> directReports = employee.getDirectReports();
+        if (directReports.size() == 0){
+            return 0;
+        } else {
+            for (Employee report: directReports){
+                totalReports += findNumReports(report);
+            }
+        }
+        return totalReports;
     }
 }
