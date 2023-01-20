e491a35ab001655f8a1e848827c751c9f4ff0617
CamNikiel <123035555+CamNikiel@users.noreply.github.com>
2023-01-20 12:23:34 -0500

    Add CompensationImpl

---
+++ src/main/java/com/mindex/challenge/service/impl/CompensationImple.java
@@ -0,0 +1,44 @@
+package com.mindex.challenge.service.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.mindex.challenge.dao.CompensationRepository;
+import com.mindex.challenge.data.Compensation;
+import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.service.CompensationService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.UUID;
+
+
+public class CompensationImple implements CompensationService{
+    private static final Logger LOG = LoggerFactory.getLogger(EmployeeServiceImpl.class);
+
+    @Autowired
+    private CompensationRepository compensationRepository;
+
+    @Override
+    public Compensation create(String employeeId) {
+        LOG.debug("Creating compensation for [{}]", employeeId);
+
+        Compensation compensation = new Compensation(employeeId);
+        compensationRepository.insert(compensation);
+
+        return compensation;
+    }
+
+    @Override
+    public Compensation read(String id) {
+        LOG.debug("Reading compensation of employee with id [{}]", id);
+
+        Compensation compensation = compensationRepository.findByEmployeeId(id);
+
+        if (compensation == null) {
+            throw new RuntimeException("Invalid employeeId: " + id);
+        }
+
+        return compensation;
+    }
+}
