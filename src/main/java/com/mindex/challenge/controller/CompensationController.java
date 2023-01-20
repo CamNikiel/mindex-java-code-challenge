a02a142ecd49ce29fa4ee87007913bee497c6403
CamNikiel <123035555+CamNikiel@users.noreply.github.com>
2023-01-20 12:20:46 -0500

    Add CompensationController

---
+++ src/main/java/com/mindex/challenge/controller/CompensationController.java
@@ -0,0 +1,34 @@
+package com.mindex.challenge.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.mindex.challenge.data.Compensation;
+import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.service.CompensationService;
+
+public class CompensationController {
+    private static final Logger LOG = LoggerFactory.getLogger(EmployeeController.class);
+
+    @Autowired
+    private CompensationService compensationService;
+
+    @PostMapping("/compensation")
+    public Compensation create(@RequestBody Employee employee) {
+        LOG.debug("Received employee create request for [{}]", employee);
+
+        return compensationService.create(employee.getEmployeeId());
+    }
+
+    @GetMapping("/compensation/{id}")
+    public Compensation read(@PathVariable String id) {
+        LOG.debug("Received compensation read request for id [{}]", id);
+        return compensationService.read(id);
+    }
+}
