d018aeab906d77b87c7f9d568e5165eb33a04f59
CamNikiel <123035555+CamNikiel@users.noreply.github.com>
2023-01-20 13:09:48 -0500

    Update CompensationController.java

--- src/main/java/com/mindex/challenge/controller/CompensationController.java
+++ src/main/java/com/mindex/challenge/controller/CompensationController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.mindex.challenge.data.Compensation;
