9842c2f7f3c4c12ac3b68b6c0f4dfba6cea4a9b8
CamNikiel <123035555+CamNikiel@users.noreply.github.com>
2023-01-22 22:03:02 -0500

    Update ReportingStructureServiceImpl.java

--- src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
@@ -1,22 +1,29 @@
 package com.mindex.challenge.service.impl;
 
-import com.mindex.challenge.data.Employee;
-import com.mindex.challenge.data.ReportingStructure;
-import com.mindex.challenge.service.ReportingStructureService;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.service.ReportingStructureService;
 
 @Service
 public class ReportingStructureServiceImpl implements ReportingStructureService {
 
     private static final Logger LOG = LoggerFactory.getLogger(EmployeeServiceImpl.class);
 
-    @Override
-    public ReportingStructure create(Employee employee) {
-        LOG.debug("Creating ReportingStructure for [{}]", employee);
+    @Autowired
+    private ReportingStructureService reportingStructureService;
 
-        return new ReportingStructure(employee);
+    @Override
+    public ReportingStructure read(String employeeId) {
+        LOG.debug("Creating ReportingStructure for [{}]", employeeId);
+
+        if (employeeId == null) {
+            throw new RuntimeException("Invalid employeeId: " + employeeId);
+        }
+        
+        return reportingStructureService.read(employeeId);
     }
 }
