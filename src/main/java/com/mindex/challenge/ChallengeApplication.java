61dbcd7cd058d12bccfa74d8f4a7bdfe6b951a24
CamNikiel <123035555+CamNikiel@users.noreply.github.com>
2023-01-22 22:13:04 -0500

    Update ChallengeApplication.java

--- src/main/java/com/mindex/challenge/ChallengeApplication.java
+++ src/main/java/com/mindex/challenge/ChallengeApplication.java
@@ -10,24 +10,4 @@ public class ChallengeApplication {
 		SpringApplication.run(ChallengeApplication.class, args);
 		
 	}
-
-	public ReportingStructure getReportingStructure(int employeeId) {
-		Employee employee = findByEmployeeId(employeeId);
-		int numOfReports = getNumOfReports(employee);
-		return new ReportingStructure(employee, numOfReports);
-	}
-
-	public int getNumOfReports(Employee employee) {
-		List<Employee> directReports = employee.getDirectReports();
-		int numOfDirectReports = directReports.size();
-		int totalReports = 0;
-		for (i = 0; i < numOfDirectReports; i++){
-			if (directReports[i].getDirectReports().size() == 0){
-				totalReports+=1;
-			} else {
-				totalReports+=1;
-				getNumOfReports(directReports[i]);
-			}
-		}
-	}
 }
